16a898f8515df1592fe5fe819a15888b7609012f
Yoshi Mejia <yoshimejia00@gmail.com>
2020-10-01 13:40:38 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,12 @@
 # code your #position_taken? method here!
+def position_taken?(board, index)
+  #if board["#{index}"] == " "
+  #else false
+  #end
+  if board[index] == " "
+  puts false
+else
+  puts "what"
+end
+
+end
